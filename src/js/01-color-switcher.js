f5a75567b44c8caff233a1712277177b2212b9e5
fenota-viktoriya <Fen9206@gmail.com>
2022-04-03 18:33:35 +0300

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const startBtnRef = document.querySelector("[data-start]");
+const stopBtnRef = document.querySelector("[data-stop]");
+const bodyRef = document.querySelector('body');
+const PROMPT_DELAY = 1000;
+startBtnRef.addEventListener('click', onChangeColor);
+stopBtnRef.addEventListener('click', onChangeColorStop);
 
+let timeId = null;
+
+function onChangeColor(){
+   onChangeColorBody();
+     timeId = setInterval (()=>{
+onChangeColorBody();
+
+},PROMPT_DELAY);
+
+startBtnRef.setAttribute('disabled',true );
+stopBtnRef.removeAttribute('disabled')
+};
+
+function onChangeColorStop(){
+    clearInterval(timeId);
+    stopBtnRef.setAttribute('disabled', true);
+startBtnRef.removeAttribute('disabled')
+};
+
+
+function onChangeColorBody(){
+    const randomColor = getRandomHexColor();
+    bodyRef.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
